07c4b54cdde919e43408280e90bc9ee67280002d
00abdoreda <56522413+00abdoreda@users.noreply.github.com>
2022-12-21 14:36:04 +0200

    Add files via upload

--- Single.cpp
+++ Single.cpp
@@ -13,6 +13,94 @@ void insertNode(int x);
 void insertatbeg(int x);
 void insertatpos(int x,int pos);
 void display();
+void deletebeg(){
+	if(head==NULL){
+		cout<<"list is empty\n";
+	}
+	else if(head==tail){
+		delete(head);
+		head=tail=NULL;
+		length--;
+	}else{
+		node*cur=head;
+		head=head->next;
+		delete(cur);
+		length--;
+		
+	}
+	
+}
+void deleteTail(){
+		if(head==NULL){
+		cout<<"list is empty\n";
+	}
+	else if(head==tail){
+		delete(head);
+		head=tail=NULL;
+		length--;
+	}else{
+	
+	node*cur=head;
+	while(cur->next!=tail){
+		cur=cur->next;
+	}
+	delete(tail);
+	tail=cur;
+	tail->next=NULL;
+	length--;
+	
+	}
+}
+void deleteElement(int x){
+	if(head==NULL){
+		cout<<"list is empty\n";
+	}else {
+		node *cur=head;
+		if(cur->data==x){
+			deletebeg();
+		}else{
+			node*prev=head;
+			cur=head->next;
+			while(cur->data!=x){
+				if(cur->next==NULL&&cur->data!=x){
+					cout<<"not found\n";
+					return ;
+				}else{
+						cur=cur->next;
+				prev=prev->next;
+				}
+			
+				
+			}
+			prev->next=cur->next;
+			delete(cur);
+			if(prev->next==NULL){
+				tail=prev;
+			}
+			length--;
+			
+		}
+	}
+}
+
+void find(int x){
+	if(head==NULL){
+		cout<<"list is empty\n";
+	}else{
+		node*cur=head;
+		while(cur->data!=x){
+			if(cur->next==NULL&&cur->data!=x){
+				cout<<"not found\n";
+				return;
+			}else{
+				
+				cur=cur->next;
+			}
+		}
+		cout<<"founded"<<endl;
+	}
+	
+}
 
 
 int main(){
@@ -22,7 +110,26 @@ int main(){
 	insertatbeg(15);
 	//15 5 7 9
 	insertatpos(20,1);
-	display();
+	//15 20 5 7 9
+	find(19);
+	//deleteElement(5);
+	//insertNode(19);
+	//20 5 7 9
+		
+	
+	
+	
+	
+	//deletebeg();
+	//20 5 7 9
+	//deletebeg();
+	//5 7 9
+	
+//	deleteTail();
+	//5 7
+
+		
+	//display();
 }
 
 
